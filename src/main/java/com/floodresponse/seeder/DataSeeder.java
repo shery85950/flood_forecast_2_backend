7e875c4c47265b2eb29f5e43be46fcfaa30500bd
shery85950 <155225747+shery85950@users.noreply.github.com>
2025-12-04 15:11:49 +0500

    Update DataSeeder.java

--- src/main/java/com/floodresponse/seeder/DataSeeder.java
+++ src/main/java/com/floodresponse/seeder/DataSeeder.java
@@ -52,6 +52,7 @@ public class DataSeeder implements CommandLineRunner {
     }
 
     private void seedShelters() {
+        // Existing Lahore shelters
         Shelter shelter1 = new Shelter();
         shelter1.setName("Government High School Model Town");
         shelter1.setAddress("Model Town, Lahore");
@@ -75,6 +76,176 @@ public class DataSeeder implements CommandLineRunner {
         shelter2.setFacilities("Food, Water");
         shelter2.setOpen(true);
         shelterRepository.save(shelter2);
+
+        // Islamabad Shelters
+        Shelter isl1 = new Shelter();
+        isl1.setName("F-6 Markaz Community Center");
+        isl1.setAddress("F-6 Markaz, Islamabad");
+        isl1.setLatitude(33.7215);
+        isl1.setLongitude(73.0433);
+        isl1.setCapacity(400);
+        isl1.setCurrentOccupancy(0);
+        isl1.setContactNumber("051-2345678");
+        isl1.setFacilities("Food, Water, Medical Aid, Blankets, Toilets");
+        isl1.setOpen(true);
+        shelterRepository.save(isl1);
+
+        Shelter isl2 = new Shelter();
+        isl2.setName("Pakistan Sports Complex");
+        isl2.setAddress("G-9/2, Islamabad");
+        isl2.setLatitude(33.6844);
+        isl2.setLongitude(73.0479);
+        isl2.setCapacity(800);
+        isl2.setCurrentOccupancy(0);
+        isl2.setContactNumber("051-9262626");
+        isl2.setFacilities("Food, Water, Medical Aid, Security, Parking");
+        isl2.setOpen(true);
+        shelterRepository.save(isl2);
+
+        Shelter isl3 = new Shelter();
+        isl3.setName("I-10 Markaz Rescue Center");
+        isl3.setAddress("I-10 Markaz, Islamabad");
+        isl3.setLatitude(33.6651);
+        isl3.setLongitude(73.0169);
+        isl3.setCapacity(350);
+        isl3.setCurrentOccupancy(0);
+        isl3.setContactNumber("051-5730000");
+        isl3.setFacilities("Food, Water, Medical Aid, Emergency Services");
+        isl3.setOpen(true);
+        shelterRepository.save(isl3);
+
+        Shelter isl4 = new Shelter();
+        isl4.setName("G-11 Government School");
+        isl4.setAddress("G-11/3, Islamabad");
+        isl4.setLatitude(33.6751);
+        isl4.setLongitude(73.0301);
+        isl4.setCapacity(450);
+        isl4.setCurrentOccupancy(0);
+        isl4.setContactNumber("051-2290123");
+        isl4.setFacilities("Food, Water, Toilets, Classrooms");
+        isl4.setOpen(true);
+        shelterRepository.save(isl4);
+
+        Shelter isl5 = new Shelter();
+        isl5.setName("Sector H-9 Community Hall");
+        isl5.setAddress("H-9/1, Islamabad");
+        isl5.setLatitude(33.6594);
+        isl5.setLongitude(73.0294);
+        isl5.setCapacity(300);
+        isl5.setCurrentOccupancy(0);
+        isl5.setContactNumber("051-4443322");
+        isl5.setFacilities("Food, Water, Blankets");
+        isl5.setOpen(true);
+        shelterRepository.save(isl5);
+
+        Shelter isl6 = new Shelter();
+        isl6.setName("Blue Area Emergency Center");
+        isl6.setAddress("Blue Area, Islamabad");
+        isl6.setLatitude(33.7077);
+        isl6.setLongitude(73.0578);
+        isl6.setCapacity(250);
+        isl6.setCurrentOccupancy(0);
+        isl6.setContactNumber("051-9203456");
+        isl6.setFacilities("Food, Water, Medical Aid, Security");
+        isl6.setOpen(true);
+        shelterRepository.save(isl6);
+
+        // Rawalpindi Shelters
+        Shelter rwp1 = new Shelter();
+        rwp1.setName("Saddar Relief Camp");
+        rwp1.setAddress("Saddar Bazaar, Rawalpindi");
+        rwp1.setLatitude(33.5975);
+        rwp1.setLongitude(73.0479);
+        rwp1.setCapacity(600);
+        rwp1.setCurrentOccupancy(0);
+        rwp1.setContactNumber("051-5551234");
+        rwp1.setFacilities("Food, Water, Medical Aid, Blankets, Security");
+        rwp1.setOpen(true);
+        shelterRepository.save(rwp1);
+
+        Shelter rwp2 = new Shelter();
+        rwp2.setName("Liaquat Bagh Emergency Shelter");
+        rwp2.setAddress("Liaquat Road, Rawalpindi");
+        rwp2.setLatitude(33.6007);
+        rwp2.setLongitude(73.0679);
+        rwp2.setCapacity(500);
+        rwp2.setCurrentOccupancy(0);
+        rwp2.setContactNumber("051-9270123");
+        rwp2.setFacilities("Food, Water, Medical Aid, Toilets, Parking");
+        rwp2.setOpen(true);
+        shelterRepository.save(rwp2);
+
+        Shelter rwp3 = new Shelter();
+        rwp3.setName("Bahria Town Community Center");
+        rwp3.setAddress("Bahria Town Phase 4, Rawalpindi");
+        rwp3.setLatitude(33.5226);
+        rwp3.setLongitude(73.1089);
+        rwp3.setCapacity(700);
+        rwp3.setCurrentOccupancy(0);
+        rwp3.setContactNumber("051-5730456");
+        rwp3.setFacilities("Food, Water, Medical Aid, Security, Generators");
+        rwp3.setOpen(true);
+        shelterRepository.save(rwp3);
+
+        Shelter rwp4 = new Shelter();
+        rwp4.setName("Chaklala Cantonment Relief Center");
+        rwp4.setAddress("Chaklala Cantonment, Rawalpindi");
+        rwp4.setLatitude(33.6161);
+        rwp4.setLongitude(73.0997);
+        rwp4.setCapacity(550);
+        rwp4.setCurrentOccupancy(0);
+        rwp4.setContactNumber("051-5092345");
+        rwp4.setFacilities("Food, Water, Medical Aid, Security, Military Support");
+        rwp4.setOpen(true);
+        shelterRepository.save(rwp4);
+
+        Shelter rwp5 = new Shelter();
+        rwp5.setName("Satellite Town Community Hall");
+        rwp5.setAddress("Satellite Town, Rawalpindi");
+        rwp5.setLatitude(33.6294);
+        rwp5.setLongitude(73.0686);
+        rwp5.setCapacity(400);
+        rwp5.setCurrentOccupancy(0);
+        rwp5.setContactNumber("051-4581234");
+        rwp5.setFacilities("Food, Water, Blankets, Toilets");
+        rwp5.setOpen(true);
+        shelterRepository.save(rwp5);
+
+        Shelter rwp6 = new Shelter();
+        rwp6.setName("Westridge Emergency Shelter");
+        rwp6.setAddress("Westridge, Rawalpindi");
+        rwp6.setLatitude(33.6528);
+        rwp6.setLongitude(73.0686);
+        rwp6.setCapacity(350);
+        rwp6.setCurrentOccupancy(0);
+        rwp6.setContactNumber("051-5123456");
+        rwp6.setFacilities("Food, Water, Medical Aid");
+        rwp6.setOpen(true);
+        shelterRepository.save(rwp6);
+
+        Shelter rwp7 = new Shelter();
+        rwp7.setName("DHA Phase 2 Relief Camp");
+        rwp7.setAddress("DHA Phase 2, Islamabad");
+        rwp7.setLatitude(33.5369);
+        rwp7.setLongitude(73.1342);
+        rwp7.setCapacity(450);
+        rwp7.setCurrentOccupancy(0);
+        rwp7.setContactNumber("051-5164567");
+        rwp7.setFacilities("Food, Water, Medical Aid, Security, Generators, Parking");
+        rwp7.setOpen(true);
+        shelterRepository.save(rwp7);
+
+        Shelter rwp8 = new Shelter();
+        rwp8.setName("Adiala Road Community Center");
+        rwp8.setAddress("Adiala Road, Rawalpindi");
+        rwp8.setLatitude(33.5642);
+        rwp8.setLongitude(73.1156);
+        rwp8.setCapacity(300);
+        rwp8.setCurrentOccupancy(0);
+        rwp8.setContactNumber("051-4890123");
+        rwp8.setFacilities("Food, Water, Blankets");
+        rwp8.setOpen(true);
+        shelterRepository.save(rwp8);
     }
 
     private void seedHelplines() {
